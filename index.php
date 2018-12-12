56985c004751f1d0a59c9df763409dfa37ea5ae2
fdlymn <45793718+fdlymn@users.noreply.github.com>
2018-12-12 20:47:31 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,444 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Desa Nusa laut</title>
+	<link href="img/garudabw.png" rel="icon">
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="css/animate.css" rel="stylesheet" />
+    <!-- Squad theme CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/hover.css" rel="stylesheet">
+	<link href="color/default.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+	<!-- Preloader -->
+	<div id="preloader">
+	  <div id="load"></div>
+	</div>
+
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="#intro">
+                	
+                    <img src="img/logonusa.png" id="logo_menu">
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+      <ul class="nav navbar-nav">
+				<li class="active"><a href="#intro">BERANDA</a></li>
+				<li><a href="#profil-desa">PROFIL DESA</a></li>
+				<li><a href="#galeri-berita">GALERI & BERITA</a></li>
+				<li><a href="#service">POTENSI DESA</a></li>
+				<li><a href="#contact">KONTAK KAMI</a></li>
+		</ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+	<!-- Section: intro -->
+    <section id="intro" class="intro">
+	
+		<div class="slogan">
+			<h2>SELAMAT DATANG DI <span class="text_color">NUSA LAUT</span> </h2>
+			<h4>kabupaten maluku tengah</h4>
+		</div>
+		<div class="page-scroll">
+			<a href="#profil-desa" class="btn btn-circle">
+				<i class="fa fa-angle-double-down animated"></i>
+			</a>
+		</div>
+    </section>
+	<!-- /Section: intro -->
+
+    
+    
+    <!-- /Section: profil desa -->
+            
+        <section id="profil-desa" class="home-section text-center">
+            
+            <div class="heading-about">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="wow bounceInDown" data-wow-delay="0.4s">
+                                <div class="section-heading">
+                                    <h2 class="color-white">Profil Desa</h2>
+                                    <i class="fa fa-2x fa-angle-down"></i>
+                                </div>
+                                </div>
+                        </div>
+                        </div>
+                    </div>
+            </div>
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-2 col-lg-offset-5">
+                    <hr class="marginbot-50">
+                </div>
+            </div>
+            <div class="wow fadeInLeft" data-wow-delay="0.6s">
+            	<div id="profildesa-con">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 font-size-normal" align="center">
+                   Nusalaut adalah sebuah kecamatan di Kabupaten Maluku Tengah, Maluku, 
+                   Indonesia. Pulau Nusalaut merupakan salah satu dari Pulau-pulau Lease. 
+                   Pulau-pulau Lease merupakan sebutan untuk kumpulan pulau yang terdiri 
+                   dari Pulau Saparua, Pulau Haruku, dan Pulau Nusalaut. Lorem ipsum dolor sit amet, 
+                   consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                     cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                   <br>
+                   <br>
+				<a href="#" class="hvr-shutter-out-vertical selengkapnya">Selengkapnya</a>
+                </div>
+            	</div>
+            </div>
+
+        </div>
+            
+        </div>  
+            
+        </section><!--end of section about us--->
+    
+	<!-- /Section: ABOUT US -->
+    
+    
+    
+	<!-- Section: galeri dan berita -->
+    <section id="galeri-berita" class="home-section text-center">
+		<div class="heading-about">
+			<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="wow bounceInDown" data-wow-delay="0.4s">
+						<div class="section-heading">
+							<h2 class="color-blue">Galeri & Berita</h2>
+							<i class="fa fa-2x fa-angle-down"></i>
+
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="container">
+
+		<div class="row">
+			<div class="col-lg-2 col-lg-offset-5">
+				<hr class="marginbot-50">
+			</div>
+		</div>
+
+        <div class="row">
+        	<div class="wow fadeInLeftBig" data-wow-delay="0.8s">
+				 <div class="col-xs-12 col-sm-5 col-md-5 galeri">
+                    <h4>Galeri</h4>
+                    <div id="img-gallery"><a href="#"><img src="img/galeri1.jpg"></a></div>
+                    <div id="img-gallery"><a href="#"><img src="img/galeri2.jpg"></a></div>
+                    <div id="img-gallery"><a href="#"><img src="img/galeri3.jpg"></a></div>
+                    <div id="img-gallery"><a href="#"><img src="img/galeri4.jpg"></a></div>
+                   
+
+
+                    <br>
+                    <br>
+                    <a href="#" class="hvr-shutter-out-vertical selengkapnya">Selengkapnya</a>
+                 	<br>
+                    <hr>
+                </div>
+            </div>
+
+				 <div class="col-xs-12 col-sm-2 col-md-2 "align="center">
+                    <div style="border:0.5px solid #ebebeb;height:500px;width:0.5px;" class="vl"></div>
+                </div>
+					
+
+
+                <div class="wow fadeInRightBig" data-wow-delay="0.8s">
+				 <div class="col-xs-12 col-sm-5 col-md-5" align="left">
+                    <h4 align="center">Berita</h4>
+
+                    <a href="#" class="color-blue"><h5>Pelantikan Pengurus Ikatan Keluarga Besar Nusahulawanno</h5></a>
+                    <p class="font-size-normal">
+                    	Masyarakat Nusalaut di Kota Ambon, pada hari ini (Jumat, 27 Desember 2013), merayakan Natal IKBN dengan meri.....<a href="#" class="color-blue">selengkapnya</a>
+                    </p>
+                    <hr>
+
+                    <a href="#" class="color-blue"><h5>Ekspedisi 1000 pulau</h5></a>
+                    <p class="font-size-normal">
+                    	“ekspedisi 1000 pulau “  adalah slogan  kegiatan sebagai apresiasi bagi Pahlawan Perempuan Mal.....<a href="#" class="color-blue">selengkapnya</a>
+                    </p>
+                    <hr>
+
+                    <a href="#" class="color-blue"><h5>e-KTP di Kecamatan Nusalaut</h5></a>
+                    <p class="font-size-normal">
+                    	berdasarkan data terkini dengan mengacu pada DPT PemiluLegislatif 2014 jumlah pemilih di Kecamatan nu.......<a href="#" class="color-blue">selengkapnya</a> </p>
+                 
+
+                    <div align="center"><a href="#" class="hvr-shutter-out-vertical selengkapnya">Selengkapnya</a></div>
+                    <hr>
+
+                </div>
+            </div>
+        </div>		
+		</div>
+	</section>
+	<!-- /Section: about -->
+    
+    
+    
+	
+	
+
+	<!-- Section: services -->
+    <section id="service" class="home-section text-center bg-url">
+		
+		<div class="heading-about">
+			<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="wow bounceInDown" data-wow-delay="0.4s">
+					<div class="section-heading">
+					<h2 class="color-blue">Potensi Desa</h2>
+					<i class="fa fa-2x fa-angle-down"></i>
+
+					</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="container">
+		<div class="row">
+			<div class="col-lg-2 col-lg-offset-5">
+				<hr class="marginbot-50">
+			</div>
+		</div>
+        <div class="row">
+
+        	<div class="wow fadeInLeftBig" data-wow-delay="0.4s">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+            	<div class="potensi-desa">
+            		<div id="img-potensi">
+
+            			<a href="#"><img src="img/potensi1.jpg"></a>
+            		</div>
+            			<br>
+            			<h4 class="color-gray">SUMBER DAYA ALAM</h4>
+            			<p class="font-size-normal color-gray">
+            				Meliputi informasi desa nusa laut di bidang
+            				<a href="#" class="color-blue">perikanan</a>, 
+            				<a href="#" class="color-blue">partanian</a>, 
+            				<a href="#" class="color-blue">perkebunan</a>,
+            				<a href="#" class="color-blue">peternakan</a>, dan 
+            				<a href="#" class="color-blue">pariwisata</a>
+            			</p>		
+						<a href="#" class="hvr-shutter-out-vertical selengkapnya">Selengkapnya</a>
+
+
+            	</div>
+            </div>
+        </div>
+
+        	<div class="wow fadeInUpBig" data-wow-delay="0.4s">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+            	<div class="potensi-desa">
+            		<div id="img-potensi">
+
+            			<a href="#"><img src="img/potensi2.jpg"></a>
+            		</div>
+            			<br>
+            			<h4 class="color-gray">SUMBER DAYA MANUSIA</h4>
+            			<p class="font-size-normal color-gray">
+            				Meliputi informasi desa nusa laut mengenai
+            				<a href="#" class="color-blue">jumlah jiwa dan KK</a>, 
+            				<a href="#" class="color-blue">Usia Penduduk</a>, 
+            				<a href="#" class="color-blue">Pendidikan Penduduk</a>, dan
+            				<a href="#" class="color-blue">Pekerjaan Penduduk</a>
+            			</p>		
+						<a href="#" class="hvr-shutter-out-vertical selengkapnya">Selengkapnya</a>
+
+
+            	</div>
+            </div>
+        </div>
+
+        	<div class="wow fadeInRightBig" data-wow-delay="0.4s">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+            	<div class="potensi-desa">
+            		<div id="img-potensi">
+
+            			<a href="#"><img src="img/potensi3.jpg"></a>
+            		</div>
+            			<br>
+            			<h4 class="color-gray">SARANA DAN PRASARANA</h4>
+            			<p class="font-size-normal color-gray">
+            				Meliputi informasi sarana & prasarana desa nusa laut meliputi
+            				<a href="#" class="color-blue">Pemerintahan</a>, 
+            				<a href="#" class="color-blue">Transportasi</a>, 
+            				<a href="#" class="color-blue">Perekonomian</a>,
+            				<a href="#" class="color-blue">Pendidikan</a>, dan 
+            				<a href="#" class="color-blue">Kesehatan</a>
+            			</p>		
+						<a href="#" class="hvr-shutter-out-vertical selengkapnya">Selengkapnya</a>
+
+
+            	</div>
+            </div>
+        	</div>
+
+        </div>		
+		</div>
+	</section>
+	<!-- /Section: services -->
+	
+
+	
+
+	<!-- Section: contact -->
+    <section id="contact" class="home-section text-center">
+		<div class="heading-contact">
+			<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="wow bounceInDown" data-wow-delay="0.4s">
+					<div class="section-heading">
+					<h2 class="color-blue">Kontak Kami</h2>
+					<i class="fa fa-2x fa-angle-down"></i>
+
+					</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="container">
+
+		<div class="row">
+			<div class="col-lg-2 col-lg-offset-5">
+				<hr class="marginbot-50">
+			</div>
+		</div>
+    <div class="row">
+
+        <div class="wow fadeInLeftBig" data-wow-delay="0.4s">
+        <div class="col-xs-12 col-sm-6 col-md-6">
+
+			<h4 align="center">Punya pertanyaan atau saran untuk Desa Nusa Laut ?</h4>
+                <form id="contact-form">
+                <div class="row">
+                    
+                        <div class="form-group">
+                            <label for="name">
+                                Nama</label>
+                            <input type="text" class="form-control" id="name" placeholder="Masuikan Nama" required="required" />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">
+                                Alamat Email</label>
+                            <div class="input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                                </span>
+                                <input type="email" class="form-control" id="email" placeholder="Masukan Email" required="required" /></div>
+                        </div>
+                        <div class="form-group">
+                            <label for="subject">
+                                Subject</label>
+                            <select id="subject" name="subject" class="form-control" required="required">
+                                <option value="na" selected="">Pilih satu:</option>
+                                <option value="saran">Saran & Pengaduan</option>
+                                <option value="Pertanyaan">Pertanyaan tentang Desa Nusa Laut</option>
+                                <option value="lainnya">Lainnya</option>
+                            </select>
+                        </div>
+                   
+                   
+                        <div class="form-group">
+                            <label for="name">
+                                Isi Pesan</label>
+                            <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
+                                placeholder="Isi Pesan"></textarea>
+                        </div>
+                  
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-skin" id="btnContactUs">
+                            Kirim Pesan</button>
+                    </div>
+                    <br>
+                    <hr>
+                </div>
+                </form>
+        </div>
+    </div>
+		
+
+        <div class="wow fadeInRightBig" data-wow-delay="0.4s">
+		<div class="col-xs-12 col-sm-6 col-md-6">
+
+			<h4 align="center">Temukan Kami di Google Map</h4>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63706.385095537364!2d128.74993188968168!3d-3.667130316895605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d6b67a43eb00367%3A0x5d191d10cde9fb21!2sNusalaut%2C+Central+Maluku+Regency%2C+Maluku!5e0!3m2!1sen!2sid!4v1544620258498" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>	
+		</div>
+		</div>
+    </div>	
+
+		</div>
+	</section>
+	<!-- /Section: contact -->
+
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-lg-12">
+					<div class="wow shake" data-wow-delay="0.4s">
+					<div class="page-scroll marginbot-30">
+						<a href="#intro" id="totop" class="btn btn-circle">
+							<i class="fa fa-angle-double-up animated"></i>
+						</a>
+					</div>
+					</div>
+					<p>&copy;Copyright 2018 - Desa Nusa Laut. All rights reserved.
+                    <!-- 
+                        All links in the footer should remain intact. 
+                        Licenseing information is available at: http://bootstraptaste.com/license/
+                        You can buy this theme without footer links online at: http://bootstraptaste.com/buy/?theme=Squadfree
+                    -->
+				</div>
+			</div>	
+		</div>
+	</footer>
+
+    <!-- Core JavaScript Files -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>	
+	<script src="js/jquery.scrollTo.js"></script>
+	<script src="js/wow.min.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
